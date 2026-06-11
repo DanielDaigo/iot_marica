72781d679283078a0fafb410d3aba88cd831e739
Daniel Daigo <daniel.daigo@example.com>
2026-06-11 00:35:48 -0300

    correção bug adicionar usuário portal admin

--- portal/apps/core/admin.py
+++ portal/apps/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+
+# 1. Desregistra o User e Group originais (que estão com o visual bugado)
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+# 2. Registra novamente forçando a interface do Unfold (Tailwind e Dark mode)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
